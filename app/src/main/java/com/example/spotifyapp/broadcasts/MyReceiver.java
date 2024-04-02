db7ea0186d647eb5926d44b4d90df560e835a235
DangVanCong2301 <dangvancong2301@gmail.com>
2024-04-02 18:06:53 +0700

    cmt in receiver.java

--- app/src/main/java/com/example/spotifyapp/broadcasts/MyReceiver.java
+++ app/src/main/java/com/example/spotifyapp/broadcasts/MyReceiver.java
@@ -16,6 +16,6 @@ public class MyReceiver extends BroadcastReceiver {
         intentService.putExtra("action_music_service", actionMusic);
         intentService.putExtra("status_player_service", statusMusic);
 
-        context.startService(intentService);
+        context.startService(intentService); // Không được gủi intent phải là intentService
     }
 }
